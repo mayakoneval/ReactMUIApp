9447dbeb7fbd7a428c3565df5fe4807155fbd72a
mayakoneval <mayakoneval@gmail.com>
2018-09-09 20:09:31 -0400

    basic react app with mui tabs

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import SimpleTabs from './TabBar';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <SimpleTabs />
+      </div>
+    );
+  }
+}
+
+export default App;
